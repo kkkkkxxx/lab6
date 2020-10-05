bc2f01b31d90fce7f520dce0b4d039fa1c63d14f
kkkkkxxx <kaleriasergeeva1@gmail.com>
2020-10-05 16:59:37 +0900

    6

---
+++ pr6_2.java
@@ -0,0 +1,30 @@
+package lab6;
+import java.io.*;
+
+public class pr6_2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = null;
+        BufferedWriter out=null;
+        try {
+// Создание файловых символьных потоков для чтения и записи
+            br = new BufferedReader( new FileReader("C:\\Users\\lera\\MyFile1.txt" ), 1024);
+            out = new BufferedWriter( new FileWriter( "C:\\Users\\lera\\MyFile2.txt" ));
+            int lineCount = 0; // счетчик строк
+            String s;
+// Переписывание информации из одного файла в другой
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+                System.out.println(lineCount + ": " + s);
+                out.write(s);
+                out.newLine(); // переход на новую строку
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка !!!!!!!!");
+        }
+        finally{
+            br.close();
+            out.flush();
+            out.close();
+        }
+    }
+}
